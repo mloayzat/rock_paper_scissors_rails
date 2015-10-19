060dc08d19dadd19b3a2a49a57eb4e68a5fd16bf
mloayzat <mloayzat@chicagobooth.edu>
2015-10-19 14:39:38 -0500

    test for class

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,9 +7,19 @@ class GameController < ApplicationController
     # The move the user chose is in the variable @user_move.
     # ===============================================================
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = ["rock", "paper", "scissors"].sample 
 
-    @result = "Replace this string with the correct value."
+    
+    if @user_move == @computer_move 
+     @result = "It's a tie!"
+        elsif @user_move == "rock" && @computer_move == "paper" || @user_move == "scissors" && @computer_move == "rock" || @user_move == "paper" && @computer_move == "scissors" 
+    @result = "lose!"
+        elsif @user_move == "scissors" && @computer_move == "paper" || @user_move == "paper" && @computer_move == "rock"  || @user_move == "rock" && @computer_move == "scissors" 
+    @result = "win!"
+
+        else @result = "are not smart, that doesn't seem to be a valid move, what were you thinking, it was supposed to be a simple game."
+
+end
 
     # ===============================================================
     # Your code goes above.
